b392a0536e6b1b69317e26e89846b7f8de15a98e
Kristof Niederholtmeyer <kristof.niederholtmeyer@rwth-aachen.de>
2014-05-10 16:14:20 +0200

    Use new options.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from Adafruit_Thermal import *
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 
-os.system("./FontPrinter -s 32 -c 65 Font.ttf > ./bitmap.py")
+os.system("./FontPrinter -s 32 -c 103 -b 20 -d 20 font.otf > ./bitmap.py")
 
 import bitmap as bm
 printer.printBitmap(bm.width, bm.height, bm.data)
