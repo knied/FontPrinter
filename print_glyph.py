41acf2c762b6777d7e6c093e952cb6653d3b3dac
Kristof Niederholtmeyer <kristof.niederholtmeyer@rwth-aachen.de>
2014-05-11 19:30:59 +0200

    Trying to get the printing more stable.

--- print_glyph.py
+++ print_glyph.py
@@ -3,11 +3,12 @@
 import subprocess, sys
 from Adafruit_Thermal import *
 
-size = 16
-base_line = 8
+size = 40
+base_line = 12
 spacing = 8
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
+printer.setTimes(120000, 8400)
 p = subprocess.Popen(["./FontPrinter", "-c", sys.argv[1], "-s", "%d" % (size), "-b", "%d" % (base_line), "-d", "%d" % (spacing), sys.argv[2]], stdout=subprocess.PIPE)
 out, err = p.communicate()
 data = out.split(",")
